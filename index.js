c180bc2096117e17e2b339b16a0c0825596f0be8
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-08-29 15:42:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    
-    if (urlParams.get('utm_source') === 'chatgpt.com') {
-        document.title = "Dumbfounded™ - You Really Used ChatGPT...?";
-    }
-});
+const urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.get('utm_source') === 'chatgpt.com') {
+    document.title = "Dumbfounded™ - You Really Used ChatGPT...?";
+}
