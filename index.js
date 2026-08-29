8451df24140cef2bb8111d46bca8b4c1e2d8e043
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-08-29 15:44:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const urlParams = new URLSearchParams(window.location.search);
 
 if (urlParams.get('utm_source') === 'chatgpt.com') {
     document.title = "Dumbfounded™ - You Really Used ChatGPT...?";
+    
+    const cleanUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+    window.history.replaceState({ path: cleanUrl }, '', cleanUrl);
+
+    alert("Caught Red-Handed!\n\nYou really used ChatGPT to get here...?");
 }
