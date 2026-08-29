00b2369df1083d6179600c46d54ee4a080f69f0d
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-08-29 15:47:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const urlParams = new URLSearchParams(window.location.search);
+window.onload = function() {
+    console.log("Dumbfounded Script Connected! Query string is:", window.location.search);
 
-if (urlParams.get('utm_source') === 'chatgpt.com') {
-    document.title = "Dumbfounded™ - You Really Used ChatGPT...?";
+    const urlParams = new URLSearchParams(window.location.search);
     
-    const cleanUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
-    window.history.replaceState({ path: cleanUrl }, '', cleanUrl);
-
-    alert("Caught Red-Handed!\n\nYou really used ChatGPT to get here...?");
-}
+    if (urlParams.get('utm_source') === 'chatgpt.com') {
+        
+        document.title = "Dumbfounded™ - You Really Used ChatGPT...?";
+        
+        alert("Seriously?\n\nYou really used ChatGPT to get here...?");
+        
+        const cleanUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        window.history.replaceState({ path: cleanUrl }, '', cleanUrl);
+    }
+};
