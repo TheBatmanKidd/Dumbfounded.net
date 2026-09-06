328b03d6111ee6995282c53705d86d390af7d51c
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-05 22:10:42 -0500

    Update script.js

--- home/script.js
+++ home/script.js
@@ -1,6 +1,8 @@
+const currentUrl = new URL(window.location.href);
+
 if (currentUrl.pathname === '/home') {
     currentUrl.search = '';
     window.history.replaceState({ path: currentUrl.href }, '', currentUrl.href);
 } else {
-    window.location.href = 'https://www.dumbfounded.net';
+    window.location.href = 'https://dumbfounded.net';
 }
