7f068ac9d9d0c59334dff13b1dee55e0d2dba93e
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-05 22:13:04 -0500

    Update and rename script.js to index.html

--- home/script.js
+++
@@ -1,8 +0,0 @@
-const currentUrl = new URL(window.location.href);
-
-if (currentUrl.pathname === '/home') {
-    currentUrl.search = '';
-    window.history.replaceState({ path: currentUrl.href }, '', currentUrl.href);
-} else {
-    window.location.href = 'https://dumbfounded.net';
-}
