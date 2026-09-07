ee6f46a108fe76468a488494c4d6365fb52b33cd
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-07 15:29:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const currentUrl = new URL(window.location.href);
 const urlParams = currentUrl.searchParams;
 
+const windowElement = document.querySelector(".window");
+
+const observer = new IntersectionObserver((entries) => {
+  const entry = entries[0];
+
+  if (!entry.isIntersecting) {
+    if (!document.querySelector(".top-left")) {
+      const topLeft = document.createElement("div");
+
+      topLeft.className = "top-left";
+      topLeft.textContent = document.title;
+
+      document.body.appendChild(topLeft);
+    }
+  }
+}, {
+  threshold: 0
+});
+
+observer.observe(windowElement);
+
+observer.observe(firstWindow);
+
 if (urlParams.get('utm_source') === 'chatgpt.com') {
     document.title = "Dumbfounded™ - ChatGPT, Really?";
 }
