deb621939d6fb232c7c4ed4a345d434e8d4a44fa
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-20 15:39:45 -0500

    Create index.js

---
+++ fonbire/index.js
@@ -0,0 +1,6 @@
+function showStuff(response) {
+    if (response.length > 0) {
+        document.getElementById('stuff').style.display = 'block';
+        document.getElementById('captcha').style.display = 'none';
+    }
+}
