9d74a354423bb2697e95288c70881d0f60eb4f3c
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-20 15:43:12 -0500

    Update index.js

--- fonbire/index.js
+++ fonbire/index.js
@@ -1,4 +1,4 @@
-function showStuff(response) {
+function revealStuff(response) {
     if (response.length > 0) {
         document.getElementById('stuff').style.display = 'block';
         document.getElementById('captcha').style.display = 'none';
