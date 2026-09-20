c67025cfca2c8e2e47582627e2a68b6c0dc82317
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-20 18:34:32 -0500

    Update index.js

--- fonbire/index.js
+++ fonbire/index.js
@@ -4,11 +4,3 @@ function revealStuff(response) {
         document.getElementById('captcha').style.display = 'none';
     }
 }
-
-if (!navigator.onLine) {
-        window.location.href = '/fonbire/offline/';
-    }
-
-window.addEventListener('offline', () => {
-    window.location.href = '/offline/';
-});
