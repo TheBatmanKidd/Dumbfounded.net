5e50282068e7757065f25dbac3761a44126cc94c
TheBatmanKidd <harrisonjostevens@gmail.com>
2026-09-24 18:42:25 -0500

    Update index.js

--- fonbire/index.js
+++ fonbire/index.js
@@ -4,3 +4,10 @@ function revealStuff(response) {
         document.getElementById('captcha').style.display = 'none';
     }
 }
+
+function revealSubmit(response) {
+    if (response.length > 0) {
+        document.getElementById('submit').style.display = 'block';
+        document.getElementById('captcha2').style.display = 'none';
+    }
+}
